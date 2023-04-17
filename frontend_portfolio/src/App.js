a7ea243fa22d6b9cdd5ce41d15f904d1096eabff
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-04-17 12:13:46 +0200

    Added navbar styling in desktop mode

--- frontend_portfolio/src/App.js
+++ frontend_portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
 import { About, Footer, Header, Projects, Skills } from './container';
 import { Navbar } from './components';
+import './App.scss'
 
 function App() {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Projects />
