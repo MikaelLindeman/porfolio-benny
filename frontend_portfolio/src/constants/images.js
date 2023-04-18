284be9dfe88c1553a09eaf388c96bd2298b5a82d
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-04-18 13:04:08 +0200

    Added correct pictures for header page

--- frontend_portfolio/src/constants/images.js
+++ frontend_portfolio/src/constants/images.js
@@ -2,9 +2,13 @@ import logo from "../assets/logo.png";
 import picture from "../assets/Picture.svg";
 import purpleBg from "../assets/Purple-bg.svg";
 import c4 from "../assets/c4.svg";
+import unity from "../assets/unity.svg";
+import jira from "../assets/jira.svg";
 export default {
   logo,
   picture,
   purpleBg,
   c4,
+  unity,
+  jira,
 };
