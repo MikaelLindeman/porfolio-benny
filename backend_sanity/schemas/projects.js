d8048e6871ee960124bf8e58004f11cd9494f0dd
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-04-24 13:10:33 +0200

    Added field to complete project pag

--- backend_sanity/schemas/projects.js
+++ backend_sanity/schemas/projects.js
@@ -1,25 +1,25 @@
 export default {
-    name: 'projects',
-    title: 'Projects',
-    type: 'document',
-    fields: [
-        {
-            name: 'name',
-            title: 'Name',
-            type: 'string'
-        },
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'string'
-        },
-        {
-            name: 'imageurl',
-            title: 'ImgUrl',
-            type: 'image',
-            options: {
-                hotspot: true,
-            }
-        }
-    ]
+  name: 'projects',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImgUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
 }
--- frontend_portfolio/src/container/Projects/Projects.jsx
+++ frontend_portfolio/src/container/Projects/Projects.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
-import './Projects.scss'
-
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Projects.scss";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [projects, setProjects] = useState([]);
+  const [filterProject, setFilterProject] = useState([]);
 
-export default Projects
+  useEffect(() => {
+    const query = `*[_type == "projects"]`;
+
+    client.fetch(query).then((data) => {
+      setProjects(data);
+      setFilterProject(data);
+    });
+  }, []);
+
+  const handleProjectFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        My creative <span>Projects</span>
+      </h2>
+      <div className="app__work-filter">
+        {["Tower Defence!", "Snake", "Robots", "Wow"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleProjectFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterProject.map((projects, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(projects.imgUrl)} alt={projects.name} />
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
+};
+
+export default Projects;
