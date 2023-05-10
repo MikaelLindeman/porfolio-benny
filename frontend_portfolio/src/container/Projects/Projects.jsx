547ca8327a07775fa6ea1db8de254855722fcdb0
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-05-10 10:28:42 +0200

    Added finishing touches before build

--- frontend_portfolio/src/container/Projects/Projects.jsx
+++ frontend_portfolio/src/container/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { AiFillEye, AiFillGithub } from "react-icons/ai";
+// import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
@@ -43,7 +43,7 @@ const Projects = () => {
         My creative <span>Projects</span>
       </h2>
       <div className="app__work-filter">
-        {["Strategy", "FPS", "Fighter", "All"].map((item, index) => (
+        {["Strategy", "RPG", "Fighter", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleProjectFilter(item)}
@@ -61,53 +61,59 @@ const Projects = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-        {filterProject.map((projects, index) => (
-          <div className="app__work-item app__flex" key={index}>
-            <div className="app__work-img app__flex">
-              <img src={urlFor(projects.imgUrl)} alt={projects.name} />
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{
-                  duration: 0.25,
-                  ease: "easeInOut",
-                  staggerChildren: 0.5,
-                }}
-                className="app__work-hover app__flex"
-              >
-                {/* Code Below only if links to project */}
-                <a href={projects.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-                <a href={projects.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-              </motion.div>
+        {filterProject &&
+          filterProject.map((projects, index) => (
+            <div className="app__work-item app__flex" key={index}>
+              <div className="app__work-img app__flex">
+                <img src={urlFor(projects.imgUrl)} alt={projects.name} />
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{
+                    duration: 0.25,
+                    ease: "easeInOut",
+                    staggerChildren: 0.5,
+                  }}
+                  className="app__work-hover app__flex"
+                >
+                  {/* Code Below only if links to project */}
 
-              {/* Code above only if linkts to project */}
-            </div>
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{projects.name}</h4>
-              <p className="p-text">{projects.description}</p>
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{projects.tags[0]}</p>
+                  {/* <a
+                    href={projects.projectLink}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                  <a href={projects.codeLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a> */}
+
+                  {/* Code above only if linkts to project */}
+                </motion.div>
+              </div>
+              <div className="app__work-content app__flex">
+                <h4 className="bold-text">{projects.name}</h4>
+                <p className="p-text">{projects.description}</p>
+                <div className="app__work-tag app__flex">
+                  <p className="p-text">{projects.tags[0]}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </motion.div>
     </>
   );
